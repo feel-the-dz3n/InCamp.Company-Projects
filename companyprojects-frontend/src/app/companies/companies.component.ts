e2e0b3f050f910c28493831b46690dda69bec2e6
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-20 19:02:24 +0200

    Create companyService and get companies list in CompaniesComponent

--- companyprojects-frontend/src/app/companies/companies.component.ts
+++ companyprojects-frontend/src/app/companies/companies.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Company} from "../company.model";
+import {CompanyService} from "../company.service";
 
 @Component({
   selector: 'app-companies',
@@ -10,8 +11,22 @@ export class CompaniesComponent implements OnInit {
   isLoading: boolean = false;
   companies: Company[] | undefined;
 
+  constructor(private companyService: CompanyService) {
+  }
+
   ngOnInit(): void {
-    // TODO: fetch companies from service here
+    this.isLoading = true;
+    this.companyService.getCompanies().subscribe(
+      response => {
+        this.companies = response;
+        this.isLoading = false;
+      },
+      error => {
+        alert('Failed to fetch companies, try again later');
+        console.log(error);
+        this.isLoading = false;
+      }
+    );
   }
 
 }
