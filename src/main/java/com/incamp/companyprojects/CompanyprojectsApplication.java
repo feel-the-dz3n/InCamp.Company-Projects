de0a4fa16725515958a3f092deff8d618d79883d
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-20 14:48:19 +0200

    Initial commit

---
+++ src/main/java/com/incamp/companyprojects/CompanyprojectsApplication.java
@@ -0,0 +1,13 @@
+package com.incamp.companyprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyprojectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyprojectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/incamp/companyprojects/CompanyprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incamp.companyprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyprojectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
