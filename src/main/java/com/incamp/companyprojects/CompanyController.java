f94688a2c3aba3a7b106525ddc11db18c669a31d
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-20 15:33:58 +0200

    Declare controller for Company, add spring-web package

---
+++ src/main/java/com/incamp/companyprojects/CompanyController.java
@@ -0,0 +1,12 @@
+package com.incamp.companyprojects;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/company")
+@CrossOrigin
+public class CompanyController {
+
+}
