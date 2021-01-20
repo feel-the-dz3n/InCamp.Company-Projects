44ddf582be2eb3dcf2a5f754f30e5c9881a65d5b
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-20 15:36:32 +0200

    Add mappings for CompanyController

--- src/main/java/com/incamp/companyprojects/CompanyController.java
+++ src/main/java/com/incamp/companyprojects/CompanyController.java
@@ -1,12 +1,22 @@
 package com.incamp.companyprojects;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/company")
 @CrossOrigin
 public class CompanyController {
+    @Autowired
+    private CompanyService companyService;
 
+    @GetMapping
+    public Iterable<Company> getCompanies() {
+        return companyService.getCompanies();
+    }
+
+    @GetMapping("/{id}")
+    public Company getCompany(@PathVariable Integer id) {
+        return companyService.getCompany(id);
+    }
 }
