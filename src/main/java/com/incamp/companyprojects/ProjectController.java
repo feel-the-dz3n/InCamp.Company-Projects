3d5f1f5148a485ac15803a2a7df1d52ce6d519a1
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-21 19:24:21 +0200

    Implement project's service and controller

--- src/main/java/com/incamp/companyprojects/ProjectController.java
+++ src/main/java/com/incamp/companyprojects/ProjectController.java
@@ -1,12 +1,22 @@
 package com.incamp.companyprojects;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 @RequestMapping(path = "/project")
 public class ProjectController {
+    @Autowired
+    private ProjectService projectService;
 
+    @GetMapping
+    public Iterable<Project> getAll() {
+        return projectService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Project get(@PathVariable int id) {
+        return projectService.get(id).get();
+    }
 }
