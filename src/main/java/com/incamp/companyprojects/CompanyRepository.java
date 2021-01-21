95a460050d292e6e3bfe9f468dc740968f3072a7
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-21 20:09:20 +0200

    Implement and use getCompanyProjects

--- src/main/java/com/incamp/companyprojects/CompanyRepository.java
+++ src/main/java/com/incamp/companyprojects/CompanyRepository.java
@@ -1,6 +1,10 @@
 package com.incamp.companyprojects;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CompanyRepository extends CrudRepository<Company, Integer> {
+    @Query(value = "SELECT company_id as companyId, id as projectId, name as projectName FROM project WHERE company_id = :companyId;", nativeQuery = true)
+    Iterable<CompanyProjectDTO> getCompanyProjects(@Param("companyId") Integer companyId);
 }
