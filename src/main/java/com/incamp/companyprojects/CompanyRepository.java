15e219024640fdc6ff5be4c35da2841020575d7d
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-21 20:18:20 +0200

    Fix getCompanyProjects

--- src/main/java/com/incamp/companyprojects/CompanyRepository.java
+++ src/main/java/com/incamp/companyprojects/CompanyRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface CompanyRepository extends CrudRepository<Company, Integer> {
-    @Query(value = "SELECT company_id as companyId, id as projectId, name as projectName FROM project WHERE company_id = :companyId;", nativeQuery = true)
+    @Query(value = "SELECT company_id as companyId, id as projectId, name as projectName FROM project WHERE company_id = :companyId", nativeQuery = true)
     Iterable<CompanyProjectDTO> getCompanyProjects(@Param("companyId") Integer companyId);
 }
