4b801b7c8e9065f1762d5402b45b68a2d4da3207
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-21 19:20:49 +0200

    Create test data SQL script

---
+++ src/main/resources/db/test_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO company VALUES(1, 'InterLink inCamp');
+
+INSERT INTO person VALUES(1, 'Yaroslav');
+INSERT INTO person VALUES(2, 'Rob');
+INSERT INTO person VALUES(3, 'Ben');
+INSERT INTO person VALUES(4, 'Bob');
+
+INSERT INTO technology VALUES(1, 'C#');
+INSERT INTO technology VALUES(2, 'Java');
+INSERT INTO technology VALUES(3, 'Hibernate');
+INSERT INTO technology VALUES(4, 'Spring');
+INSERT INTO technology VALUES(5, 'HTML5');
+INSERT INTO technology VALUES(6, 'JavaScript');
+INSERT INTO technology VALUES(7, 'Angular');
+
+INSERT INTO project VALUES(1, 'To-Do List', 1);
+INSERT INTO project VALUES(2, 'TicTacToe', 1);
