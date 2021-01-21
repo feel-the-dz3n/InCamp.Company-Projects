c981ef9ef419d0227ff3f0f401296f3b0d98368c
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-21 18:29:37 +0200

    Add Project model

---
+++ src/main/java/com/incamp/companyprojects/Project.java
@@ -0,0 +1,67 @@
+package com.incamp.companyprojects;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@SequenceGenerator(
+        name = "project-id-gen",
+        sequenceName = "project_id_seq",
+        allocationSize = 1)
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "project-id-gen")
+    private Integer id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Company company;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Technology> technologies;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Person> people;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public List<Technology> getTechnologies() {
+        return technologies;
+    }
+
+    public void setTechnologies(List<Technology> technologies) {
+        this.technologies = technologies;
+    }
+
+    public List<Person> getPeople() {
+        return people;
+    }
+
+    public void setPeople(List<Person> people) {
+        this.people = people;
+    }
+}
