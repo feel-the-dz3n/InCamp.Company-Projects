1eef40d55473271be3173aae6375d90bec57bfb1
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-21 19:38:22 +0200

    Improve test-data

--- src/main/resources/db/test_data.sql
+++ src/main/resources/db/test_data.sql
@@ -1,3 +1,5 @@
+TRUNCATE company, project, technology, project_technologies, project_people, person;
+
 INSERT INTO company VALUES(1, 'InterLink inCamp');
 
 INSERT INTO person VALUES(1, 'Yaroslav');
@@ -14,4 +16,18 @@ INSERT INTO technology VALUES(6, 'JavaScript');
 INSERT INTO technology VALUES(7, 'Angular');
 
 INSERT INTO project VALUES(1, 'To-Do List', 1);
+INSERT INTO project_people (project_id, people_id) VALUES(1, 1);
+INSERT INTO project_people (project_id, people_id) VALUES(1, 3);
+INSERT INTO project_technologies (project_id, technologies_id) VALUES (1, 2);
+INSERT INTO project_technologies (project_id, technologies_id) VALUES (1, 3);
+INSERT INTO project_technologies (project_id, technologies_id) VALUES (1, 4);
+INSERT INTO project_technologies (project_id, technologies_id) VALUES (1, 5);
+INSERT INTO project_technologies (project_id, technologies_id) VALUES (1, 6);
+INSERT INTO project_technologies (project_id, technologies_id) VALUES (1, 7);
+
 INSERT INTO project VALUES(2, 'TicTacToe', 1);
+INSERT INTO project_people (project_id, people_id) VALUES(2, 1);
+INSERT INTO project_people (project_id, people_id) VALUES(2, 2);
+INSERT INTO project_technologies (project_id, technologies_id) VALUES (2, 2);
+
+
