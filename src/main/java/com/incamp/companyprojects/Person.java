f71734abd960c49738357da6e495894973d5f1cf
Yaroslav <yanet.prod@gmail.com>
2021-01-22 11:12:11 +0300

    Extend Person model

--- src/main/java/com/incamp/companyprojects/Person.java
+++ src/main/java/com/incamp/companyprojects/Person.java
@@ -1,6 +1,10 @@
 package com.incamp.companyprojects;
 
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @SequenceGenerator(
@@ -12,6 +16,14 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "person-id-gen")
     private Integer id;
     private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Technology> skills;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Contribution> contributions;
 
     public Integer getId() {
         return id;
@@ -27,6 +39,22 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Technology> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Technology> skills) {
+        this.skills = skills;
+    }
+
+    public List<Contribution> getContributions() {
+        return contributions;
+    }
+
+    public void setContributions(List<Contribution> contributions) {
+        this.contributions = contributions;
     }
 }
 
