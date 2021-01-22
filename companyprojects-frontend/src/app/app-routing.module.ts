d0499b9bc3db4fc9bef8c2df09bf681d488456cd
Yaroslav <yanet.prod@gmail.com>
2021-01-22 13:28:33 +0200

    Add route

--- companyprojects-frontend/src/app/app-routing.module.ts
+++ companyprojects-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CompaniesComponent } from "./companies/companies.component";
 import { CompanyComponent } from "./company/company.component";
+import { PersonComponent } from "./person/person.component";
 
 const routes: Routes = [
   { path: 'companies', component: CompaniesComponent },
   { path: 'company/:id', component: CompanyComponent },
+  { path: 'person/:id', component: PersonComponent },
   { path: '', redirectTo: '/companies', pathMatch: 'full' },
 ];
 
