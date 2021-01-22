5733b9e5112893bee520bd35c0dba9ab8d9f4d8c
Yaroslav <yanet.prod@gmail.com>
2021-01-22 16:04:04 +0200

    Implement ProjectService.get

--- companyprojects-frontend/src/app/project.service.ts
+++ companyprojects-frontend/src/app/project.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Project} from "./project.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
+  private endpoint: string = 'http://127.0.0.1:8080/project'
 
   constructor(private http: HttpClient) { }
+
+  get(id: any) {
+    return this.http.get<Project>(this.endpoint + `/${id}`);
+  }
 }
