dbe81afa3e6d08279f1d682e2a3fcf816d58e519
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-25 17:21:35 +0200

    Update person model

--- companyprojects-frontend/src/app/person.model.ts
+++ companyprojects-frontend/src/app/person.model.ts
@@ -1,7 +1,9 @@
 import {Technology} from "./technology.model";
+import {Company} from "./company.model";
 
 export interface Person {
   id: number | undefined;
   name: string | undefined;
   skills: Technology[] | undefined;
+  membership?: Company[];
 }
