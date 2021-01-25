1c7536952505d17bb5d4632222680a125c89a342
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-25 17:22:17 +0200

    Implement getMembershipProject API request

--- companyprojects-frontend/src/app/people.service.ts
+++ companyprojects-frontend/src/app/people.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Person} from "./person.model";
 import {ContributionProject} from "./contribution-project.model";
+import {Project} from "./project.model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,15 @@ import {ContributionProject} from "./contribution-project.model";
 export class PeopleService {
   private endpoint: string = 'http://127.0.0.1:8080/people';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   get(personId: any) {
     return this.http.get<Person>(this.endpoint + `/${personId}`);
+  }
+
+  getMembershipProject(personId: any) {
+    return this.http.get<Project[]>(this.endpoint + `/${personId}/membership-projects`);
   }
 
   getContributions(personId: any) {
