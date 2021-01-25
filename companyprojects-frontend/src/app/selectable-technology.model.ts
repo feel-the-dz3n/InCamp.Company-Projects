9b89e1e2f71a3c1dfd27cca61f4bffa0429e7a01
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-25 15:15:25 +0200

    Add SelectableTechnology

---
+++ companyprojects-frontend/src/app/selectable-technology.model.ts
@@ -0,0 +1,10 @@
+import {Technology} from "./technology.model";
+
+export class SelectableTechnology {
+  selected: boolean = false;
+  technology?: Technology;
+
+  constructor(technology: Technology) {
+    this.technology = technology;
+  }
+}
