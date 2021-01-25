8ae9f440003e342c2195c4c6d6048fbfaec96404
Yaroslav Kibysh <yanet.prod@gmail.com>
2021-01-25 14:13:16 +0200

    Refactor Contribution + migrate

--- src/main/java/com/incamp/companyprojects/Contribution.java
+++ src/main/java/com/incamp/companyprojects/Contribution.java
@@ -1,7 +1,11 @@
 package com.incamp.companyprojects;
 
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @SequenceGenerator(
@@ -11,14 +15,61 @@ import java.time.LocalDateTime;
 public class Contribution {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "contrib-id-gen")
-    Integer id;
+    private Integer id;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    Project project;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Project project;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    Person person;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Person person;
 
-    LocalDateTime startDate;
-    LocalDateTime endDate;
+    @JoinTable(name = "contrib_technologies",
+            joinColumns = @JoinColumn(name = "contrib_id"),
+            inverseJoinColumns = @JoinColumn(name = "technology_id"))
+    @ManyToMany(fetch = FetchType.LAZY)
+    @LazyCollection(LazyCollectionOption.FALSE)
+    private List<Technology> technologies;
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
 }
